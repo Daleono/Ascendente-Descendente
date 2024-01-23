f5e0126a77a1f8bb4f06a67fd6a7c8b809b36fb9
Daleono <david.gonzalez15818@gmail.com>
2024-01-22 23:51:05 -0600

    Agregado código de ejecución

---
+++ Ejecutar.java
@@ -0,0 +1,11 @@
+package Disenio_Interfaces;
+
+public class Ejecutar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Ascendente_Descendente V = new Ascendente_Descendente();
+		
+	}
+
+}
